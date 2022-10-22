2b4966776aa6f37c08c17474941e7dd2332f9e0c
Irfan Alfiansyah <irfan.alfiansyah@moladin.com>
2022-10-22 18:53:18 +0700

    init marketplace frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import Sidebar from "@/components/Sidebar";
+import Head from "next/head";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, hiddenSidebar }) {
+  return (
+    <>
+      <Head>
+        <meta name="google" content="notranslate" />
+      </Head>
+      {hiddenSidebar ? (
+        <Component {...pageProps} />
+      ) : (
+        <Sidebar>
+          <Sidebar.MainContent>
+            <Component {...pageProps} />
+          </Sidebar.MainContent>
+        </Sidebar>
+      )}
+    </>
+  );
 }
 
-export default MyApp
+MyApp.getInitialProps = async (appContext) => {
+  const { pathname } = appContext.ctx;
+  return { hiddenSidebar: pathname === "/auth/login" || pathname === "/auth/register" ? true : false };
+};
+
+export default MyApp;
--- src/components/AuthPages/Login.js
+++ src/components/AuthPages/Login.js
@@ -179,7 +179,7 @@ export default function LoginModal({ open, setOpen }) {
                           class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
                         >
                           <svg
-                            class="mr-2 -ml-1 w-4 h-4"
+                            className="mr-2 -ml-1 w-4 h-4"
                             aria-hidden="true"
                             focusable="false"
                             data-prefix="fab"
