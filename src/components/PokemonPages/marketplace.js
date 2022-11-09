9197c9ed455ebfed1703a8a0e9da40226bff853b
v-reza <mrezalf0@gmail.com>
2022-11-09 15:07:57 +0700

    delete bug routing be

--- src/components/PokemonPages/marketplace.js
+++
@@ -1,7 +0,0 @@
-const { recentSales } = require("../../controllers/MarketplaceController");
-
-const router = require("express").Router();
-
-router.get("/recent-sales", recentSales);
-
-module.exports = router;
